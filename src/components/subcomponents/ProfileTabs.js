4a5b20f593f12739e36821a2cb888b22b59f551a
L EE <lee@MacBook-Pro-2.local>
2021-08-17 20:59:18 +0100

    members area modal added ratings,some elements missing in history and styling in bookings etc

--- src/components/subcomponents/ProfileTabs.js
+++ src/components/subcomponents/ProfileTabs.js
@@ -13,6 +13,21 @@ import CalendarTodayIcon from '@material-ui/icons/CalendarToday';
 import HistoryIcon from '@material-ui/icons/History';
 import StarHalfIcon from '@material-ui/icons/StarHalf';
 
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Grid from '@material-ui/core/Grid';
+import EditIcon from '@material-ui/icons/Edit';
+import { Divider, Tooltip, Button, Container, TextField } from '@material-ui/core';
+import { StylesContext } from '@material-ui/styles/StylesProvider';
+
+import Rating from '@material-ui/lab/Rating';
+
+
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
 
@@ -49,14 +64,44 @@ function a11yProps(index) {
 const useStyles = makeStyles((theme) => ({
   root: {
     backgroundColor: theme.palette.background.paper,
-    
+  },
+  list: {
+    flexGrow: 1,
+    maxWidth: 752,
+  },
+  listGroup: {
+    backgroundColor: theme.palette.background.paper,
+  },
+  editButton: {
+    color: '#5d5d5d',
+  },
+  small: {
+    width: theme.spacing(3),
+    height: theme.spacing(3),
+  },
+  large: {
+    width: theme.spacing(7),
+    height: theme.spacing(7),
+  },
+  centered: {
+    textAlign: 'center'
+  },
+  formSubmit: {
+    marginTop: '1em'
   },
 }));
+
+
 
 export default function FullWidthTabs() {
   const classes = useStyles();
   const theme = useTheme();
   const [value, setValue] = React.useState(0);
+
+  const [dense, setDense] = React.useState(false);
+  const [secondary, setSecondary] = React.useState(false);
+
+  const [rating, setRating] = React.useState(0);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -73,14 +118,13 @@ export default function FullWidthTabs() {
           value={value}
           onChange={handleChange}
           indicatorColor="primary"
-          textColor="primary"
           variant="fullWidth"
           aria-label="full width tabs example"
         >
-          <Tab label="Profile" {...a11yProps(0)} icon={<AccountCircleIcon/>} />
-          <Tab label="Appointments" {...a11yProps(1)} icon={<CalendarTodayIcon/>}/>
+          <Tab label="Edit Profile" {...a11yProps(0)} icon={<AccountCircleIcon/>} />
+          <Tab label="My Bookings" {...a11yProps(1)} icon={<CalendarTodayIcon/>}/>
           <Tab label="Booking History" {...a11yProps(2)} icon={<HistoryIcon/>}/>
-          <Tab label="Rating" {...a11yProps(3)} icon={<StarHalfIcon/>}/>
+          <Tab label="Give Rating" {...a11yProps(3)} icon={<StarHalfIcon/>}/>
         </Tabs>
       </AppBar>
       <SwipeableViews
@@ -90,15 +134,100 @@ export default function FullWidthTabs() {
       >
         <TabPanel value={value} index={0} dir={theme.direction}>
             
+
+            <Grid item xs={12} md={6}>
+          
+          <div className={classes.listGroup}>
+            <List dense={dense}>
+              
+                <ListItem>
+                  <ListItemAvatar>
+                    <Tooltip title="enter your name">
+                        <Avatar>
+                            N 
+                        </Avatar>
+                    </Tooltip>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary="[Member Name]"
+                    secondary={secondary ? 'Secondary text' : null}
+                  />
+                  <ListItemSecondaryAction>
+                    <IconButton edge="end" aria-label="delete">
+                      <EditIcon className={classes.editButton}/>
+                    </IconButton>
+                  </ListItemSecondaryAction>
+                </ListItem>
+                <Divider/>
+                <ListItem>
+                  <ListItemAvatar>
+                    <Tooltip title="select a usual service">
+                        <Avatar>
+                            S 
+                        </Avatar>
+                    </Tooltip>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary="[Usual Service]"
+                    secondary={secondary ? 'Secondary text' : null}
+                  />
+                  <ListItemSecondaryAction>
+                    <IconButton edge="end" aria-label="delete">
+                      <EditIcon className={classes.editButton} />
+                    </IconButton>
+                  </ListItemSecondaryAction>
+                </ListItem>
+              
+            </List>
+          </div>
+        </Grid>
+
         </TabPanel>
         <TabPanel value={value} index={1} dir={theme.direction}>
-          Item Two
+            <Container className={classes.centered}>
+                <Typography variant="">
+                You have no upcoming appointments
+                <Button variant="contained" color="secondary">
+                    Book an Appointment
+                </Button>
+                </Typography>
+            </Container>
         </TabPanel>
         <TabPanel value={value} index={2} dir={theme.direction}>
           Item Three
         </TabPanel>
         <TabPanel value={value} index={3} dir={theme.direction}>
-          Item Four
+        <Box component="fieldset" mb={3} borderColor="transparent" className={classes.centered}>
+        <Typography>
+            "Good experience? We would really appreciate your feedback"
+        </Typography>
+        <Rating
+          name="simple-controlled"
+          value={rating}
+          onChange={(event, newValue) => {
+            event.preventDefault()
+            setRating(newValue);
+          }}
+          size="large"
+        />
+        
+      </Box>
+      <Container className={classes.centered}>
+      <TextField
+          id="outlined-multiline-flexible"
+          label="Comments"
+          multiline
+          maxRows={4}
+          /*value={}*/
+          /*onChange={}*/
+          variant="outlined"
+        />
+        </Container>
+        <Container className={classes.centered}>
+        <Button variant="contained" color="secondary" className={classes.formSubmit}>
+           Send
+        </Button>
+        </Container>
         </TabPanel>
       </SwipeableViews>
     </div>
